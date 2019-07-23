837703d4ccafe67caf2f2371fb90f16a0882e06f
Pradeep <mailtobaral@gmail.com>
2019-07-22 17:22:31 -0700

    Added bootstrap in  django with data loaded from dictionary and login register page

--- fonereview/fonereview/urls.py
+++ fonereview/fonereview/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
 from users import views as User_views
 from app import views as App_views
