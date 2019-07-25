80656c4ea1973b83b72405681d8c6cbbbb840d5c
Ali Berenjian <ali.rberenjian@gmail.com>
2019-07-24 17:15:58 -0700

    Change Max Length to 255

--- app/models.py
+++ app/models.py
@@ -3,31 +3,31 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Device(models.Model):
-  name = models.CharField(max_length=100)
-  brand = models.CharField(max_length=100)
-  technology = models.CharField(max_length=100)
-  gprs = models.CharField(max_length=100)
-  edge = models.CharField(max_length=100)
-  announced = models.CharField(max_length=100)
-  status = models.CharField(max_length=100)
-  weight = models.CharField(max_length=100)
-  sim = models.CharField(max_length=100)
-  device_type = models.CharField(max_length=100)
-  size = models.CharField(max_length=100)
-  resolution = models.CharField(max_length=100)
-  card_slot = models.CharField(max_length=100)
-  loud_speaker = models.CharField(max_length=100)
-  wlan = models.CharField(max_length=100)
-  bluetooth = models.CharField(max_length=100)
-  cpu = models.CharField(max_length=100)
-  battery_c = models.CharField(max_length=100)
-  sensor = models.CharField(max_length=100)
-  gpu = models.CharField(max_length=100)
-  internal_memory = models.CharField(max_length=100)
-  os = models.CharField(max_length=100)
+  name = models.CharField(max_length=255)
+  brand = models.CharField(max_length=255)
+  technology = models.CharField(max_length=255)
+  gprs = models.CharField(max_length=255)
+  edge = models.CharField(max_length=255)
+  announced = models.CharField(max_length=255)
+  status = models.CharField(max_length=255)
+  weight = models.CharField(max_length=255)
+  sim = models.CharField(max_length=255)
+  device_type = models.CharField(max_length=255)
+  size = models.CharField(max_length=255)
+  resolution = models.CharField(max_length=255)
+  card_slot = models.CharField(max_length=255)
+  loud_speaker = models.CharField(max_length=255)
+  wlan = models.CharField(max_length=255)
+  bluetooth = models.CharField(max_length=255)
+  cpu = models.CharField(max_length=255)
+  battery_c = models.CharField(max_length=255)
+  sensor = models.CharField(max_length=255)
+  gpu = models.CharField(max_length=255)
+  internal_memory = models.CharField(max_length=255)
+  os = models.CharField(max_length=255)
   image = models.FileField(upload_to="device_images/",null=True,blank=True)
   total_rating = models.DecimalField(max_digits=2,decimal_places=1,null=True,blank=True)
-  #change the var to 100
+  #change the var to 255
 
   def __str__(self):
     return self.name
@@ -54,7 +54,7 @@ class Rate(models.Model):
 
 
 class Comment(models.Model):
-  description = models.CharField(max_length=160)
+  description = models.CharField(max_length=255)
 
   user = models.ForeignKey(
     User,
